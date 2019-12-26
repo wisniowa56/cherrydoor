fc91d6c6e5d349418157857a92cdb791428a3669
opliko <25460763+oplik0@users.noreply.github.com>
2019-12-27 00:03:52 +0100

    don't use remote sources and add sri

---
+++ static/js/form.js
@@ -0,0 +1,12 @@
+$('.modal-link').click(e => {
+    $("#"+e.target.attributes["data-modal"].value).modal("show");
+    $("#"+e.target.attributes["data-modal"].value).submit(e => {
+        e.preventDefault();
+        $.ajax({
+            url:e.target.attributes["data-url"].value,
+            type:e.target.attributes["data-type"].value,
+            data:$(e.target).serialize()
+        });
+        $(".modal.show").modal("hide")
+    });
+})
