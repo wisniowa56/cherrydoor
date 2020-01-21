04ac1f15aa1546bb5b109da2b81d442bd7552f4a
opliko <25460763+oplik0@users.noreply.github.com>
2020-01-21 20:10:11 +0100

    move auth requirement setting to mongo and add break times

--- interface/commands.py
+++ interface/commands.py
@@ -1,43 +1,51 @@
 import datetime as dt
-
+from datetimerange import DateTimeRange
 from interface import read, write, mongo, config, interface
 
 
 class Commands:
-    def __init__(self, pipe):
+    def __init__(self):
         self.commands = {
             "CARD": self.card,
         }
-        self.pipe = pipe
-        if self.pipe.poll():
-            self.require_auth = self.pipe.recv()
+        try:
+            self.require_auth = bool(mongo.settings.find_one({'setting':'require_auth'})['value'])
             write(f"NTFY {4 if self.require_auth else 3}")
-        else:
+        except KeyError:
             self.require_auth = True
 
     def start(self):
+        #message template - a two element list (command and argument)
         message = ["", ""]
         with interface:
+            #continuously read the interface until EXIT is sent
             while message == [] or message[0] != "EXIT":
                 message = read().upper().split()
+                #after a newline, do the specidied command
                 try:
                     self.commands[message[0]](message[1])
                 except (KeyError, IndexError):
                     pass
-                except BrokenPipeError:
-                    break
 
     def card(self, block0):
-        if self.pipe.poll():
-            self.require_auth = self.pipe.recv()
+        try:
+            #check if authentication with UID is required, or manufacturer code is fine
+            self.require_auth = self.check_auth()
+            #send a contol signal for LEDs
             write(f"NTFY {4 if self.require_auth else 3}")
+        except KeyError:
+            #default to requiring authentication
+            self.require_auth = True
         if self.require_auth:
+            #check if card is associated with an user
             auth = bool(mongo.users.count_documents({"cards": block0[:10]}))
         else:
             try:
+                #if authentication is not required check manufacturer code - 2 last digits of block0
                 auth = block0[-2:] == config["manufacturer-code"]
             except KeyError:
                 auth = False
+        #add attempt to logs
         mongo.logs.insert(
             {
                 "timestamp": dt.datetime.now(),
@@ -47,4 +55,25 @@ class Commands:
                 "success": auth,
             }
         )
+        #send the authentication result over the interface
         write(f"AUTH {1 if auth else 0}")
+    def check_auth(self):
+        time = dt.datetime.now()
+        try:
+            #get the list of break times from mongodb
+            breaks = list(mongo.settings.find_one({'setting':'break_times'})['value'])
+            #get the current setting
+            require_auth = mongo.settings.find_one({'setting':'require_auth'})
+            #if the current setting was set manually - don't check the time
+            if not bool(require_auth['manual']):
+                for item in breaks:
+                    #if current time is in one of the time ranges, return False, so auth is not required
+                    if time in DateTimeRange(item[0], item[1]):
+                        return False
+                else:
+                    return True
+            else:
+                return bool(require_auth['value'])
+        except KeyError:
+            #default to requiring auth
+            return True
