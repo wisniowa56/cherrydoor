58c25fdd7664cba825fcfd9a58f6b68db64d9e7d
opliko <25460763+oplik0@users.noreply.github.com>
2020-01-21 22:45:12 +0100

    working card addition to users

--- static/js/form.js
+++ static/js/form.js
@@ -1,13 +1,16 @@
 "user strict";
-$('.modal-link').click(e => {
-    $("#"+e.target.attributes["data-modal"].value).modal("show");
-    $("#"+e.target.attributes["data-modal"].value).submit(e => {
-        e.preventDefault();
-        $.ajax({
-            url:e.target.attributes["data-url"].value,
-            type:e.target.attributes["data-type"].value,
-            data:$(e.target).serialize()
-        });
-        $(".modal.show").modal("hide")
+$(".modal-link").click(e => {
+  $("#" + e.target.attributes["data-modal"].value).modal("show");
+  $("#" + e.target.attributes["data-modal"].value).submit(e => {
+    if (!e.target.attributes["class"].value.includes("modal-form")) {
+      return;
+    }
+    e.preventDefault();
+    $.ajax({
+      url: e.target.attributes["data-url"].value,
+      type: e.target.attributes["data-type"].value,
+      data: $(e.target).serialize()
     });
-})
+    $(".modal.show").modal("hide");
+  });
+});
--- static/js/users.js
+++ static/js/users.js
@@ -6,23 +6,41 @@ apiSocket.on("users", json => {
   json.forEach(value => {
     users.push(JSON.parse(value));
   });
+  var userContainer = $("#userContainer");
+  userContainer.html("");
   var html;
   users.forEach(user => {
     html = `<a class="list-group-item user-item" id="user-${user.username}" href="#collapseUser${user.username}" data-toggle="collapse" datadata-username="${user.username} aria-expanded="false" aria-controls="collapseUser${user.username}">${user.username}</a>
         <div class="collapse" id="collapseUser${user.username}">
-            <ul class="list-group" id="cardList${user.username}>
+            <ul class="list-group" id="cardList${user.username}">
+            <li><form class="add-card-form form-inline w-100" id="addCard${user.username}Form" data-url="/api/card" data-type="POST">
+                <input type="hidden" name="username" value="${user.username}">
+                <input class="form-control card-input" id="addCard${user.username}" type="text" placeholder="ID Karty" name="card" pattern="[0-9a-fA-F]{10}">
+                <button class="btn btn-primary card-form-button" type="submit">Dodaj</button>
+            </form></li>
             </ul>
         </div>`;
-    $("#userContainer").append(html);
+    userContainer.append(html);
+    var cardlist = $(`#cardList${user.username}`);
     user.cards.forEach(card => {
-      html = `<li class="list-group-item" id="cardItem${user.username}Id${card}"><form ${card}</li>`;
-      $(`#cardList${user.username}`).append(html);
+      html = `<li class="list-group-item card-item" id="cardItem${card}">${card}</li>`;
+      cardlist.append(html);
+    });
+  });
+  $(".add-card-form").submit(e => {
+    e.preventDefault();
+    $.ajax({
+      url: e.target.attributes["data-url"].value,
+      type: e.target.attributes["data-type"].value,
+      data: $(e.target).serialize(),
+      complete: setTimeout(() => {
+        apiSocket.emit("users");
+      }, 100)
     });
   });
 });
 $(document).ready(() => {
   $("#manageUsersModal").on("show.bs.modal", () => {
-    console.log("shown");
     apiSocket.emit("users");
   });
 });
