5208fe52d3d422981db41c2c27678157cc2927e6
opliko <25460763+oplik0@users.noreply.github.com>
2020-01-29 23:17:24 +0100

    refactor frontend too

--- static/js/modals.js
+++ static/js/modals.js
@@ -1,16 +1,19 @@
 "user strict";
+const actions = {
+  closeModal: () => $(".modal.show").modal("hide"),
+  emitUsers: setTimeout(() => {
+    socket.emit("users");
+  })
+};
 $(".modal-link").click(e => {
   $("#" + e.target.attributes["data-modal"].value).modal("show");
   $("#" + e.target.attributes["data-modal"].value).submit(e => {
-    if (!e.target.attributes["class"].value.includes("modal-form")) {
-      return;
-    }
     e.preventDefault();
     $.ajax({
       url: e.target.attributes["data-url"].value,
       type: e.target.attributes["data-type"].value,
-      data: $(e.target).serialize()
+      data: $(e.target).serialize(),
+      complete: actions[e.target.attributes["data-action"].value]
     });
-    $(".modal.show").modal("hide");
   });
 });
--- static/js/users.js
+++ static/js/users.js
@@ -1,7 +1,6 @@
 "user strict";
-var apiSocket = io.connect("/api");
 
-apiSocket.on("users", json => {
+socket.on("users", json => {
   var users = [];
   json.forEach(value => {
     users.push(JSON.parse(value));
@@ -15,7 +14,7 @@ apiSocket.on("users", json => {
         <a class="text-primary btn" id="user-${user.username}" href="#collapseUser${user.username}" data-toggle="collapse" datadata-username="${user.username} aria-expanded="false" aria-controls="collapseUser${user.username}">${user.username}</a>
         </div>
         <div class="btn-group">
-        <form class="delete-user-form form-inline pull-right" id="deleteUser${user.username}Form" data-url="/api/user" data-type="DELETE">
+        <form class="delete-user-form form-inline pull-right" id="deleteUser${user.username}Form" data-url="/api/user" data-type="DELETE" data-action="emitUsers">
         <input type="hidden" name="username" value="${user.username}">
         <button class="btn btn-primary btn-sm delete-user-button" type="submit"><i class="material-icons">delete</i></button>
         </form>
@@ -24,7 +23,7 @@ apiSocket.on("users", json => {
         </li>
         <div class="collapse" id="collapseUser${user.username}">
             <ul class="list-group" id="cardList${user.username}">
-            <li><form class="add-card-form form-inline w-100 pull-right" id="addCard${user.username}Form" data-url="/api/card" data-type="POST">
+            <li><form class="add-card-form form-inline w-100 pull-right" id="addCard${user.username}Form" data-url="/api/card" data-type="POST" data-action="emitUsers>
                 <input type="hidden" name="username" value="${user.username}">
                 <input class="form-control card-input" id="addCard${user.username}" type="text" placeholder="ID Karty" name="card" pattern="[0-9a-fA-F]{10}">
                 <button class="btn btn-primary card-form-button" type="submit"><i class="material-icons">add_box</i></button>
@@ -48,31 +47,9 @@ apiSocket.on("users", json => {
       cardlist.append(html);
     });
   });
-  $(".add-card-form").submit(e => {
-    e.preventDefault();
-    $.ajax({
-      url: e.target.attributes["data-url"].value,
-      type: e.target.attributes["data-type"].value,
-      data: $(e.target).serialize(),
-      complete: setTimeout(() => {
-        apiSocket.emit("users");
-      }, 100)
-    });
-  });
-  $(".delete-user-form,.delete-card-form").submit(e => {
-    e.preventDefault();
-    $.ajax({
-      url: e.target.attributes["data-url"].value,
-      type: e.target.attributes["data-type"].value,
-      data: $(e.target).serialize(),
-      complete: setTimeout(() => {
-        apiSocket.emit("users");
-      }, 100)
-    });
-  });
 });
 $(document).ready(() => {
   $("#manageUsersModal").on("show.bs.modal", () => {
-    apiSocket.emit("users");
+    socket.emit("users");
   });
 });
