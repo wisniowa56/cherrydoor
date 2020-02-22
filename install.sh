ea96ba5b72e80db135a50943bec7c3e4dfd1d7f6
Opliko <25460763+oplik0@users.noreply.github.com>
2020-02-22 17:32:51 +0000

    MongoDB installation for Ubuntu on arm instead

--- install.sh
+++ install.sh
@@ -20,9 +20,11 @@ fi;
 if hash mongo &>/dev/null; then
     echo -e "\e[92mMongoDB jest zainstalowane\e[39m";
 else
-    echo -e "\e[93mMongoDB niezainstalowane. Instalowanie...\e[39m"
+    echo -e "\e[93mMongoDB niezainstalowane. Instalowanie...\e[39m";
+    wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | $SUDO apt-key add - &&
+    $SUDO echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | $SUDO tee /etc/apt/sources.list.d/mongodb-org-4.2.list &&
     $SUDO apt-get update &&
-    $SUDO apt-get install -y mongodb-server;
+    $SUDO apt-get install mongodb-org &&
     $SUDO service mongod start;
 fi;
 if hash pip3 &>/dev/null; then
