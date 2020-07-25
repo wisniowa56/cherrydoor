6d1097d5538bbcf855d098b404bc70bae285ce92
opliko <25460763+oplik0@users.noreply.github.com>
2020-07-26 01:00:07 +0200

    Fix #42 - don't go back by a timezone offset on each save

--- cherrydoor/static/js/settings.js
+++ cherrydoor/static/js/settings.js
@@ -1,10 +1,10 @@
 "user strict";
 if (typeof socket == "undefined") {
-  var socket = io.connect("/api");
+	var socket = io.connect("/api");
 }
 moment.locale("pl");
 function fillHtml(id) {
-  return `
+	return `
 	<div id="breakTime${id}">
 <div class="row breakTimeInput input-group" id="breakTimeInput${id}">
 		<label class="control-label col" for="breakTimeInputFrom">Od</label>
@@ -26,96 +26,97 @@ function fillHtml(id) {
 </div><hr></div>`;
 }
 const addDeletion = () => {
-  $(".deleteBreakTime").click((e) => {
-    e.preventDefault();
-    $(e.delegateTarget.attributes["data-target"].value).remove();
-  });
+	$(".deleteBreakTime").click((e) => {
+		e.preventDefault();
+		$(e.delegateTarget.attributes["data-target"].value).remove();
+	});
 };
 var timeId = 0;
 socket.on("break_times", (json) => {
-  if (!json.length) {
-    $("#breakTimesContainer").html("");
-    return;
-  }
-  var break_times = [
-    ...JSON.parse(json).map((break_time) => {
-      return [new Date(break_time[0]), new Date(break_time[1])];
-    }),
-  ];
-  var breakTimesContainer = $("#breakTimesContainer");
-  breakTimesContainer.html("");
+	if (!json.length) {
+		$("#breakTimesContainer").html("");
+		return;
+	}
+	var break_times = JSON.parse(json).map((break_time) => {
+		return [
+			moment(break_time[0]).tz("Europe/Warsaw"),
+			moment(break_time[1]).tz("Europe/Warsaw"),
+		];
+	});
+	var breakTimesContainer = $("#breakTimesContainer");
+	breakTimesContainer.html("");
 
-  break_times.forEach((break_time) => {
-    breakTimesContainer.append(fillHtml(++timeId));
-    let datePickerFrom = $(`#breakTimeInputFrom${timeId}`);
-    let datePickerTo = $(`#breakTimeInputTo${timeId}`);
-    datePickerFrom.datetimepicker({
-      format: "LT",
-      defaultDate: break_time[0],
-    });
-    datePickerTo.datetimepicker({
-      format: "LT",
-      defaultDate: break_time[1],
-    });
-    datePickerFrom.on("change.datetimepicker", (e) => {
-      datePickerTo.datetimepicker("minDate", e.date);
-    });
-  });
-  addDeletion();
+	break_times.forEach((break_time) => {
+		breakTimesContainer.append(fillHtml(++timeId));
+		let datePickerFrom = $(`#breakTimeInputFrom${timeId}`);
+		let datePickerTo = $(`#breakTimeInputTo${timeId}`);
+		datePickerFrom.datetimepicker({
+			format: "LT",
+			defaultDate: break_time[0],
+		});
+		datePickerTo.datetimepicker({
+			format: "LT",
+			defaultDate: break_time[1],
+		});
+		datePickerFrom.on("change.datetimepicker", (e) => {
+			datePickerTo.datetimepicker("minDate", e.date);
+		});
+	});
+	addDeletion();
 });
 
 $(document).ready(() => {
-  $.fn.datetimepicker.Constructor.Default = $.extend(
-    {},
-    $.fn.datetimepicker.Constructor.Default,
-    {
-      icons: {
-        time: "far fa-clock",
-        date: "far fa-calendar",
-        up: "fas fa-sort-up",
-        down: "fas fa-sort-down",
-        previous: "far fa-chevron-left",
-        next: "far fa-chevron-right",
-        today: "far fa-calendar-check-o",
-        clear: "far fa-trash",
-        close: "far fa-times",
-      },
-    }
-  );
-  $("#settingsModal").on("show.bs.modal", () => {
-    socket.emit("break_times");
-  });
-  $("#addBreak").click(() => {
-    var breakTimesContainer = $("#breakTimesContainer");
-    breakTimesContainer.append(fillHtml(++timeId));
-    let datePickerFrom = $(`#breakTimeInputFrom${timeId}`);
-    let datePickerTo = $(`#breakTimeInputTo${timeId}`);
-    datePickerFrom.datetimepicker({
-      format: "LT",
-    });
-    datePickerTo.datetimepicker({
-      format: "LT",
-    });
-    datePickerFrom.on("change.datetimepicker", (e) => {
-      datePickerTo.datetimepicker("minDate", e.date);
-    });
-    addDeletion();
-  });
-  $("#submitSettings").click(() => {
-    var breakTimes = [];
-    $(".breakTimeInputFrom").each((i, el) => {
-      if (!!$(el).datetimepicker("date")) {
-        breakTimes[i] = [$(el).datetimepicker("date")];
-      }
-    });
-    $(".breakTimeInputTo").each((i, el) => {
-      if (!!breakTimes[i] && !!$(el).datetimepicker("date")) {
-        breakTimes[i].push($(el).datetimepicker("date"));
-      }
-    });
-    if (!breakTimes.length) {
-      breakTimes = [[null, null]];
-    }
-    socket.emit("break_times", breakTimes);
-  });
+	$.fn.datetimepicker.Constructor.Default = $.extend(
+		{},
+		$.fn.datetimepicker.Constructor.Default,
+		{
+			icons: {
+				time: "far fa-clock",
+				date: "far fa-calendar",
+				up: "fas fa-sort-up",
+				down: "fas fa-sort-down",
+				previous: "far fa-chevron-left",
+				next: "far fa-chevron-right",
+				today: "far fa-calendar-check-o",
+				clear: "far fa-trash",
+				close: "far fa-times",
+			},
+		}
+	);
+	$("#settingsModal").on("show.bs.modal", () => {
+		socket.emit("break_times");
+	});
+	$("#addBreak").click(() => {
+		var breakTimesContainer = $("#breakTimesContainer");
+		breakTimesContainer.append(fillHtml(++timeId));
+		let datePickerFrom = $(`#breakTimeInputFrom${timeId}`);
+		let datePickerTo = $(`#breakTimeInputTo${timeId}`);
+		datePickerFrom.datetimepicker({
+			format: "LT",
+		});
+		datePickerTo.datetimepicker({
+			format: "LT",
+		});
+		datePickerFrom.on("change.datetimepicker", (e) => {
+			datePickerTo.datetimepicker("minDate", e.date);
+		});
+		addDeletion();
+	});
+	$("#submitSettings").click(() => {
+		var breakTimes = [];
+		$(".breakTimeInputFrom").each((i, el) => {
+			if (!!$(el).datetimepicker("date")) {
+				breakTimes[i] = [$(el).datetimepicker("date")];
+			}
+		});
+		$(".breakTimeInputTo").each((i, el) => {
+			if (!!breakTimes[i] && !!$(el).datetimepicker("date")) {
+				breakTimes[i].push($(el).datetimepicker("date"));
+			}
+		});
+		if (!breakTimes.length) {
+			breakTimes = [[null, null]];
+		}
+		socket.emit("break_times", breakTimes);
+	});
 });
