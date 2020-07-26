c359471bca2405f77f658f76100a117c3fa4cff2
opliko <25460763+oplik0@users.noreply.github.com>
2020-07-26 02:04:15 +0200

    bump version to 0.5

--- cherrydoor/server/routes.py
+++ cherrydoor/server/routes.py
@@ -12,7 +12,7 @@ from cherrydoor.server import app, db, hasher, LoginForm, login_manager, User, j
 
 __author__ = "opliko"
 __license__ = "MIT"
-__version__ = "0.5b1"
+__version__ = "0.5"
 __status__ = "Prototype"
 
 # dashboard page
