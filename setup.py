42d8409a1a82696ca232f799b69a13710aab860c
oplik0 <25460763+oplik0@users.noreply.github.com>
2020-08-12 23:00:47 +0200

    chore: change requirements to ones for aiohttp

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 setup(
     name="Cherrydoor",
     version=__version__,
-    author="opliko",
+    author="oplik0",
     description=(
         "An overengineered rfid lock manager created for my school community. Made for Raspberry Pi connected with another microcontroler that send and recieved rfid data via UART"
     ),
@@ -43,21 +43,13 @@ setup(
     entry_points={"console_scripts": ["cherrydoor = cherrydoor.cli:cherrydoor"]},
     python_requires=">=3.7",
     install_requires=[
+        "aiohttp>=3.6",
+        "aioserial>=1.3",
         "argon2-cffi>=19",
-        "DateTimeRange>=0.6",
-        "Flask>=1",
-        "Werkzeug>1",
-        "Flask-WTF>=0.14",
-        "Flask-Login>=0.4",
-        "Flask-PyMongo>=2.3",
-        "Flask-RESTful>=0.3",
-        "Flask-SocketIO>=4.2",
-        "flask-talisman>=0.7",
-        "aioserial>=1.3.0",
-        "motor>=2.1.0",
-        "pymongo>=3.10",
-        "eventlet",
+        "motor>=2.1",
+        "confuse>=1.3",
     ],
+    extras_require={"speedups": ["aiodns>=1.1", "Brotli", "cchardet",]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Education",
